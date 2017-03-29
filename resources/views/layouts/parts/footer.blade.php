f068f8950af502a23011a048ee03cf43058d41e8
dungnv <dungnv@asiantech.vn>
2017-03-29 20:31:46 +0700

    all source code from change template

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -1,3 +1,50 @@
-<footer id="footer">
-	<!-- <p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p> -->
-</footer>
+<div id="main-container-footer">
+	<div class="container-footer">
+				
+					<div id="row-1f">
+						<div class="text-row-1f"><span style="font-weight:600;font-size:15px;color:#666;line-height:250%;text-transform:uppercase;letter-spacing:1.5px;">What is Platz</span><br>Platz is a blog showcasing hand-picked free themes, design stuff, free fonts and other resources for web designers.</div>
+					</div>
+					
+					<div id="row-2f">
+						<div class="text-row-2f"><span style="font-weight:600;font-size:15px;color:#666;line-height:250%;text-transform:uppercase;letter-spacing:1.5px;">How does it work</span><br>Platz offers you all the latest freebies found all over the fourth corners without to pay.</div>
+					</div>
+					
+					<div id="row-3f">
+						<div class="text-row-3f"><span style="font-weight:600;font-size:15px;color:#666;line-height:250%;text-transform:uppercase;letter-spacing:1.5px;">Get in touch!</span><br>Subscribe our RSS or follow us on Facebook, Google+, Pinterest or Dribbble to keep updated.</div>
+					</div>
+					
+					<div id="row-4f">
+						<div class="text-row-4f"><span style="font-weight:600;font-size:15px;color:#666;line-height:250%;text-transform:uppercase;letter-spacing:1.5px;">Newsletter</span><br>You will be informed monthly about the latest content avalaible.</div>
+
+			<div id="main_tip_newsletter"> 
+				<form>
+					<input type="text" name="newsletter" id="tip_newsletter_input" list="newsletter" autocomplete=off required>
+				</form>
+			</div>
+					</div>
+					
+	</div>
+</div>
+
+
+<div id="wrapper-copyright">
+	<div class="copyright">
+		<div class="copy-text object">Copyright © 2017 by <a style="color:#D0D1D4;" href="https://www.facebook.com/dungnguyen.itdn">Dung Nguyen Van</a></div>
+		
+	<div class="wrapper-navbouton">
+			<div class="google object">
+				<a href="https://www.youtube.com/channel/UCNxGRBHI_I8SGmd3zqM8KcA">
+					<i class="fa fa-youtube-square" aria-hidden="true"></i>
+				</a>
+			</div>
+			<div class="facebook object">
+				<a href="https://www.facebook.com/dungnguyen.itdn">
+					<i class="fa fa-facebook-square" aria-hidden="true"></i>
+				</a>
+			</div>
+			<div class="linkin object">
+				<a href="https://github.com/at-dungnguyen2"><i class="fa fa-github" aria-hidden="true"></i></a>
+			</div>
+		</div>
+	</div>
+</div>
